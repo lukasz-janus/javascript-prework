cc929bcf6f27dd82297bce147952e74e5934dd1b
Łukasz Janus <lukasz.janus212@gmail.com>
2022-11-06 16:22:51 +0100

    Remove JS script for player move and result. Fix variable value and message

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = randomNumber;
 
 if(randomNumber == 1){
   computerMove = 'kamień';
@@ -11,47 +11,4 @@ if(randomNumber == 1){
 } else if(randomNumber == 3){
   computerMove = 'nożyce';
 }
-printMessage('Mój ruch to: ' + computerMove);
-
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if(playerInput == 2){
-  playerMove = 'papier';
-} else if(playerInput == 3){
-  playerMove = 'nożyce';
-}
-
-printMessage('Twój ruch to: ' + playerMove);
-
-
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Tym razem przegrywasz :(');
-} else if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis');
-}
-if( computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis');
-} else if( computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-} else if( computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Tym razem przegrywasz :(');
-}
-if( computerMove == 'nożyce' && playerMove == 'papier'){
-    printMessage('Tym razem przegrywasz :(');
-} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Remis');
-} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-}
-if(playerMove == 'nieznany ruch'){
-    printMessage('Wpisz poprawną liczbę z zakresu 1-3');
-}
+printMessage('Twój przeciwnik wybrał: ' + computerMove);
