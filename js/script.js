d84b97ef5400f793e5b204469b064c3a50142a9f
Łukasz Janus <lukasz.janus212@gmail.com>
2022-11-06 16:36:12 +0100

    Fix problem with not correct number entered by player

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = playerInput;
+let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
