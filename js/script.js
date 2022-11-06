1822d18598868627130611b5e4af1d38ec6eda03
Łukasz Janus <lukasz.janus212@gmail.com>
2022-11-06 15:58:15 +0100

    Add JS script for result

--- js/script.js
+++ js/script.js
@@ -30,3 +30,28 @@ if(playerInput == '1'){
 
 printMessage('Twój ruch to: ' + playerMove);
 
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Tym razem przegrywasz :(');
+} else if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis');
+}
+if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Tym razem przegrywasz :(');
+}
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Tym razem przegrywasz :(');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}
+if(playerMove == 'nieznany ruch'){
+    printMessage('Wpisz poprawną liczbę z zakresu 1-3');
+}
