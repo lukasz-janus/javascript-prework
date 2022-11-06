63d3a07bc24fa07ff41655ad352967c606d0a82b
Łukasz Janus <lukasz.janus212@gmail.com>
2022-11-06 14:51:00 +0100

    Remove JS variables: computerMove, playerMove

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let computerMove = `kamień`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove = `kamień`;
-
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
